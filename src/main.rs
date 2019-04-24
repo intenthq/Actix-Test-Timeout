86d5ca88938a7842c4bfd980a7663f61d4639af6
Kit <kit.maywood@intenthq.com>
2019-04-24 10:35:01 +0100

    Add example timeout test

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ impl Handler<Incrementer> for ExampleStruct {
     }
 }
 
-fn index(req: &HttpRequest<AppState>) -> impl Responder {
+fn increment(req: &HttpRequest<AppState>) -> impl Responder {
     let ex: &Addr<ExampleStruct> = &req.state().example;
 
     ex.send(Incrementer { by: 1 })
@@ -49,9 +49,43 @@ fn main() {
         App::with_state(AppState {
             example: addr.clone(),
         })
-        .resource("/", |r| r.f(index))
+        .resource("/", |r| r.f(increment))
     })
     .bind("127.0.0.1:8088")
     .unwrap()
     .run();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::test::TestServer;
+    use actix_web::{http, HttpMessage};
+    use std::str;
+
+    fn test_server() -> TestServer {
+        TestServer::build_with_state(|| {
+            let addr = ExampleStruct { count: 0 }.start();
+            AppState {
+                example: addr.clone(),
+            }
+        })
+        .start(|app| {
+            app.resource("/", |r| r.f(increment));
+        })
+    }
+
+    #[test]
+    fn increment_counter() {
+        let mut srv = test_server();
+
+        let request = srv.client(http::Method::GET, &"/").finish().unwrap();
+
+        let response = srv.execute(request.send()).unwrap();
+        assert_eq!(response.status(), 200);
+
+        let bytes = srv.execute(response.body()).unwrap();
+        let body = str::from_utf8(&bytes).unwrap();
+        assert_eq!(body, "1");
+    }
 }
